377d3970270fe1b2f9328878421073c8d4381565
Chinwilla <bogdandrozd123@gmail.com>
2024-01-26 14:53:26 +0000

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,26 +1,10 @@
-const apartment = {
-  imgUrl: "https://via.placeholder.com/640x480",
-  descr: "Spacious apartment in the city center",
-  rating: 4.7,
-  price: 5000,
-  tags: ["premium", "promoted", "top", "trusted"],
-  owner: {
-    name: "Henry Sibola",
-    phone: "982-126-1588",
-    email: "henry.carter@aptmail.com",
-  },
-};
+function addOverNum() {
+  
+}
 
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location = {
-    country: "Jamaica",
-    city: "Kingston"
-};
 
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location = {
-  country: "Jamaica",
-  city: "Kingston"
-};
+
+console.log(addOverNum(50, 15, 27)); // 0
+console.log(addOverNum(10, 12, 4, 11, 48, 10, 8)); // 71
+console.log(addOverNum(15, 32, 6, 13, 19, 8)); // 51
+console.log(addOverNum(20, 74, 11, 62, 46, 12, 36)); // 218
